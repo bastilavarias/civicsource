533acc9ef6fadc4cf6bf9a82029bb4ed4e389dd0
Sebastian <sebastiancurtislavarias@gmail.com>
2021-12-27 23:37:58 +0800

    save progress.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,3 @@
 <template>
-  <div id="app">
-    <figure class="bg-gray-100 rounded-xl p-8">
-      <img class="w-32 h-32 r" alt="" width="384" height="512" />
-      <div class="pt-6 space-y-4">
-        <blockquote>
-          <p class="text-lg">
-            “Tailwind CSS is the only framework that I've seen scale on large
-            teams. It’s easy to customize, adapts to any design, and the build
-            size is tiny.”
-          </p>
-        </blockquote>
-        <figcaption>
-          <div>Sarah Dayan</div>
-          <div>Staff Engineer, Algolia</div>
-        </figcaption>
-      </div>
-    </figure>
-  </div>
+  <router-view></router-view>
 </template>
-
-<script>
-export default {
-  name: "App",
-};
-</script>
-
-<style lang="scss" scoped></style>
---
+++ src/views/Detail.vue
@@ -0,0 +1,45 @@
+<template>
+  <section
+    :style="{ backgroundImage: `url(${require('@/assets/properties/1.png')})` }"
+    class="h-screen w-screen bg-no-repeat bg-cover bg-center relative"
+  >
+    <div class="absolute top-0 left-0 w-full h-1/5">
+      <div class="w-full h-4/5 bg-white opacity-80"></div>
+      <div class="w-full h-1/4 flex justify-between">
+        <div class="w-2/12 bg-white flex justify-between items-center px-3">
+          <div>
+            <div class="text-xs font-semibold">Outbid</div>
+            <div class="text-xs font-semibold text-red-400">12</div>
+          </div>
+          <div>
+            <div class="text-xs font-semibold">Active</div>
+            <div class="text-xs font-semibold text-gray-400">12</div>
+          </div>
+          <div>
+            <div class="text-xs font-semibold">Winning</div>
+            <div class="text-xs font-semibold text-green-400">12</div>
+          </div>
+        </div>
+
+        <div class="w-8/12 flex-shrink-0 bg-white opacity-40"></div>
+
+        <div class="w-2/12 bg-white flex justify-between items-center px-3">
+          <div>
+            <div class="text-xs font-semibold">Outbid</div>
+            <div class="text-xs font-semibold text-red-400">&#36; 100,000</div>
+          </div>
+          <div>
+            <div class="text-xs font-semibold">Active</div>
+            <div class="text-xs font-semibold text-gray-400">&#36; 100,000</div>
+          </div>
+          <div>
+            <div class="text-xs font-semibold">Winning</div>
+            <div class="text-xs font-semibold text-green-400">
+              &#36; 100,000
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
