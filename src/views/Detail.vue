f9d91b972b3fb80b8cb572e4da0c382d140df24e
Sebastian <sebastiancurtislavarias@gmail.com>
2021-12-29 14:46:37 +0800

    save progress.

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -3,9 +3,9 @@
     :style="{ backgroundImage: `url(${require('@/assets/properties/1.png')})` }"
     class="h-screen w-screen bg-no-repeat bg-cover bg-center relative relative overflow-hidden"
   >
-    <div class="absolute top-0 left-0 w-full h-1/5 4xl:h-1/6">
+    <div class="absolute top-0 left-0 w-full h-1/4 4xl:h-1/6">
       <div
-        class="w-full h-3/4 4xl:h-4/5 bg-white opacity-80 flex justify-around items-center overflow-auto"
+        class="w-full h-2/3 4xl:h-4/5 bg-white opacity-80 flex justify-around items-center overflow-auto"
       >
         <div>
           <svg
@@ -178,15 +178,15 @@
           <g></g>
         </svg>
       </div>
-      <div class="w-full h-1/4 4xl:h-1/4 flex justify-between font-medium">
+      <div class="w-full h-1/3 4xl:h-1/4 flex justify-between font-medium">
         <div
           class="w-2/12 flex-grow bg-white flex justify-between items-center px-3 overflow-auto"
         >
-          <div>
+          <div class="mr-3">
             <div>Outbid</div>
             <div class="text-red-400">12</div>
           </div>
-          <div>
+          <div class="mr-3">
             <div>Active</div>
             <div class="text-secondary-gray">12</div>
           </div>
@@ -205,8 +205,8 @@
               14 Flood St.
             </div>
             <div>
-              <span class="text-green-400 mr-5"><span>+</span>&#36; 100</span>
-              <span class="font-light text-primary-gray">&#36; 5,000</span>
+              <span class="text-green-400 mr-5"><span>+</span>&#36;100</span>
+              <span class="font-light text-primary-gray">&#36;5,000</span>
             </div>
           </div>
 
@@ -216,24 +216,24 @@
               14 Flood St.
             </div>
             <div>
-              <span class="text-primary-red mr-5"><span>-</span>&#36; 100</span>
-              <span class="font-light text-primary-gray">&#36; 5,000</span>
+              <span class="text-primary-red mr-5"><span>-</span>&#36;100</span>
+              <span class="font-light text-primary-gray">&#36;5,000</span>
             </div>
           </div>
 
           <div>
             <div><status-icon status="active"></status-icon> 14 Flood St.</div>
             <div>
-              <span class="mr-5">&#36; 100</span>
-              <span class="font-light text-primary-gray">&#36; 5,000</span>
+              <span class="mr-5">&#36;100</span>
+              <span class="font-light text-primary-gray">&#36;5,000</span>
             </div>
           </div>
 
           <div>
             <div><status-icon status="winning"></status-icon> 14 Flood St.</div>
             <div>
-              <span class="text-green-400 mr-5"><span>+</span>&#36; 100</span>
-              <span class="font-light text-primary-gray">&#36; 5,000</span>
+              <span class="text-green-400 mr-5"><span>+</span>&#36;100</span>
+              <span class="font-light text-primary-gray">&#36;5,000</span>
             </div>
           </div>
         </div>
@@ -241,17 +241,17 @@
         <div
           class="w-2/12 flex-grow bg-white flex justify-between items-center px-3 overflow-auto"
         >
-          <div>
+          <div class="mr-3">
             <div>Outbid</div>
-            <div class="text-red-400">&#36; 100,000</div>
+            <div class="text-red-400">&#36;100,000</div>
           </div>
-          <div>
+          <div class="mr-3">
             <div>Active</div>
-            <div class="text-secondary-gray">&#36; 100,000</div>
+            <div class="text-secondary-gray">&#36;100,000</div>
           </div>
-          <div>
+          <div class="mr-3">
             <div>Winning</div>
-            <div class="text-green-400">&#36; 100,000</div>
+            <div class="text-green-400">&#36;100,000</div>
           </div>
         </div>
       </div>
